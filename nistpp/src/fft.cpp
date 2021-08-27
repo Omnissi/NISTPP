a1dfa47c1fcaab24a138cd56a45110926f5c4aba
Negodyaev Sergey <Sergey.Negodyaev@infotecs.ru>
2021-08-27 16:43:58 +0300

    Перемещение файлов

--- nistpp/src/fft.cpp
+++ nistpp/src/fft.cpp
@@ -1,10 +1,11 @@
 #include <nistpp/tests.h>
 
-#include <nistpp/dfft.h>
+#include "dfft.h"
 
 #include <valarray>
 #include <complex>
 #include <cmath>
+#include <unordered_map>
 
 namespace nistpp
 {
@@ -23,26 +24,40 @@ int reverse(int num, int lg_n) {
     return res;
 }
 
-void fft(CArray &x)
+CArray fft(CArray &x)
 {
-    const size_t N = x.size();
-    if (N <= 1) return;
+    std::unordered_map<size_t, Complex> cash;
 
-    // divide
-    CArray even = x[std::slice(0, N/2, 2)];
-    CArray  odd = x[std::slice(1, N/2, 2)];
-
-    // conquer
-    fft(even);
-    fft(odd);
-
-    // combine
-    for (size_t k = 0; k < N/2; ++k)
+    size_t counter = 0;
+    const double tmp = 2 * M_PI / static_cast<double>(x.size());
+    CArray res(x.size());
+    auto calcValue = [&](size_t k, size_t n) -> Complex
     {
-        Complex t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
-        x[k    ] = even[k] + t;
-        x[k+N/2] = even[k] - t;
+        const size_t mp = k * n;
+        auto it = cash.find(mp);
+        if(it == cash.end())
+        {
+            auto res = Complex(std::cos(tmp * mp), std::sin(tmp * mp));
+            cash[mp] = res;
+            return res;
+        }
+        else
+        {
+            ++counter;
+            return it->second;
+        }
+    };
+
+    const auto N1 = x.size() - 1;
+    for(size_t k = 0; k < N1; ++k)
+    {
+        for(size_t n = 0; n < N1; ++n)
+        {
+            res[k] += x[n] * calcValue(k, n);
+        }
     }
+
+    return res;
 }
 
 return_t FftTest(const BitsStorage &data)
@@ -58,7 +73,7 @@ return_t FftTest(const BitsStorage &data)
         const auto ind_i = i * BitsStorage::numberOfBitsInWord;
         for(std::size_t j = 0; j < BitsStorage::numberOfBitsInWord; ++j)
         {
-            x[ind_i + j] = word[BitsStorage::numberOfBitsInWord - 1 - j] ? 1 : -1;//Complex(1, 0) : Complex(0, -1);
+            x[ind_i + j] = word[BitsStorage::numberOfBitsInWord - 1 - j] ? 1 : -1;
         }
     }
 
@@ -73,7 +88,7 @@ return_t FftTest(const BitsStorage &data)
 
     for(size_t i = 0; i < N/2; ++i)
     {
-        m[i+1] = sqrt(pow(x[2*i+1],2)+pow(x[2*i+2],2));
+        m[i+1] = /*std::fabs(x[i+1]);*/sqrt(pow(x[2*i+1],2)+pow(x[2*i+2],2));
     }
     size_t count = 0;
     auto upperBound = std::sqrt(2.995732274*N);
